aa76ac76c4415aedfabf5123a1dc46a2885b44d1
Shivi-pixel-740 <ShivangniS582@stemcollegiate.ca>
2025-05-14 12:18:22 -0400

    Create Lesson 6_.py

---
+++ Lesson 6_.py
@@ -0,0 +1,19 @@
+tvShow = input("Enter your favorite TV show: ")
+if tvShow == "My little pony": 
+            print("Who is your favorite character?")
+else:
+     print("You don't like My Little Pony?")
+
+TwlightSparkle = input("Who is your favorite character?") 
+print ("Twlight Sparkle, SHINY!")
+
+if TwlightSparkle == "You like her?": 
+                  print("YOU MUST LIKE HER!")
+
+yes = input("Do you actually like her?")
+if yes == "GOOD": 
+            print("YOU ARE A GOOD PERSON")
+if TwlightSparkle == "Is she purple or pink?":
+      print("She is purple and a little pink")
+elif TwlightSparkle == "Does she have wings?":
+       print("She does have wings")
